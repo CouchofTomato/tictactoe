05d803b894d4f997c716aa6f54be1d5dbc8858e3
Austin P Mason <austinpmason@gmail.com>
2015-11-20 14:25:34 +0000

    start again

--- tictactoe.rb
+++ tictactoe.rb
@@ -3,26 +3,9 @@ module Tictactoe
 	end
 
 	class Player
-		def initialize name
-			@name = name
-		end
 	end
 
 	class Game
-		def initialize
-			get_player_names
-		end
-
-		def get_player_names
-			puts "Enter player1 name"
-			player1_name = gets.chomp
-			player1 = Player.new(player1_name)
-			
-			puts "Enter player12 name"
-			player2_name = gets.chomp
-			player2 = Player.new(player2_name)
-		end
 	end
 end
 
-Tictactoe::Game.new
