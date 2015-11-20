9c0ce0455786820a4d139b9f345d291075915bf1
Austin P Mason <austinpmason@gmail.com>
2015-11-20 14:43:15 +0000

    update methods

--- tictactoe.rb
+++ tictactoe.rb
@@ -1,8 +1,30 @@
 module Tictactoe
-	class Board
-	end
 
 	class Player
+		# A player should have a name and a piece (X or O)
+		# They should be instance variables
+		def initialize name, piece
+			@name = name
+			@piece = piece
+		end
+	end
+
+	class Board
+		# Use a hash to hold the board position pieces
+		# Should be able to read and write to the array
+		attr_accessor :board
+		def initialize
+			@board = {:1 => nil,
+					:2 => nil,
+					:3 => nil,
+					:4 => nil,
+					:5 => nil,
+					:6 => nil,
+					:7 => nil,
+					:8 => nil,
+					:9 => nil
+					}
+		end
 	end
 
 	class Game
