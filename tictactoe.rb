e008671c52e0a4a295edec444e08ab918111f3ee
Austin P Mason <austinpmason@gmail.com>
2015-12-03 15:32:48 +0000

    update

--- tictactoe.rb
+++ tictactoe.rb
@@ -39,13 +39,25 @@ module Tictactoe
 		end
 
 		def get_player_information
-			puts "Player 1. Please enter your name?"
+			puts "Player 1. Please enter your name."
 			player1_name = gets.chomp
 			puts "Would you like to be X or O"
-			player1_piece = gets.upcase.chomp
+			player1_piece = gets.chomp.upcase
+			while !player1_piece.eql?("X") || !player1_piece.eql?("O")
+				puts "Please enter X or O"
+				player1_piece = gets.chomp.upcase
+			end
+			puts "Player 2. Please enter your name."
+			player2_name = gets.chomp
+			player2_piece = ""
+			player1_piece == "X" ? player2_piece = "O" : player2_piece = "X"
+			player1 = createPlayer(player1_name, player1_piece)
+			player2 = createPlayer(player2_name, player2_piece)
 
+			puts player1.to_s
+			puts player2.to_s
 		end
 	end
 end
 
-Tictactoe::Board.new.display_board
+Tictactoe::Game.new.get_player_information
